2efbf1e0ff28643aa98fab198e7858463ecf34da
Adrian Dapprich <adrian@dapprich.dev>
2020-12-08 13:11:21 +0100

    move main logic into lib

--- bin/main.ml
+++ bin/main.ml
@@ -1,35 +1,11 @@
 open Base
 open Autosubst_lib
 
-module H = Hsig
-
-let readSignature () =
-  let open Lwt.Syntax in
-  let open Lwt_io in
-  Lwt_main.run (
-    let* input = open_file ~mode:Input "./signatures/sysf_cbv.sig" in
-    read input
-  )
-
-let mainProg () =
-  let open ErrorM.Syntax in
-  let open ErrorM in
-  (* parse arguments *)
-  (* parse input HOAS *)
-  let* spec = readSignature () |> SigParser.parse_signature in
-  let* signature = SigAnalyzer.build_signature spec in
-  (* generate dot graph *)
-  let* code = GenCode.runGenCode signature in
-  (* write file *)
-  pure code
-  (* pure "" *)
-
 let main () =
   let open ErrorM in
-  let result = match run (mainProg ()) with
+  let result = match run (Program.main ()) with
     | Ok x -> x
     | Error x -> x in
   Stdio.print_endline result
 
 let () = main ()
-(* let () = print_endline "helloe" *)
