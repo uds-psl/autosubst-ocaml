4fa1781208761064b47548dac73fc455dabd91dd
Adrian Dapprich <adrian@dapprich.dev>
2021-05-24 17:09:45 +0200

    change static files in kathrin's case study.
    Probably did something wrong there. Atm it does not compile

--- case-studies/kathrin/coq/unscoped.v
+++ case-studies/kathrin/coq/unscoped.v
@@ -3,7 +3,11 @@
 Version: December 11, 2019.
  *)
 
-Require Import axioms.
+(* Adrian:
+ I changed this library a bit to work better with my generated code.
+ 1. I use nat directly instead of defining fin to be nat and using Some/None as S/O
+ 2. I removed the "s, sigma" notation for scons because it interacts with dependent function types "forall x, A"*)
+Require Import core.
 
 Definition ap {X Y} (f : X -> Y) {x y : X} (p : x = y) : f x = f y :=
   match p with eq_refl => eq_refl end.
@@ -13,26 +17,17 @@ Definition apc {X Y} {f g : X -> Y} {x y : X} (p : f = g) (q : x = y) : f x = g 
 
 (** ** Primitives of the Sigma Calculus. *)
 
-(* Definition None := 0. *)
-(* Definition Some := S. *)
-
-(* Notation fin := nat. *)
 Definition shift  := S.
 
 Definition var_zero := 0.
 
-Definition id {X} (x: X) := x.
+Definition id {X} := @Datatypes.id X.
 
 Definition scons {X: Type} (x : X) (xi : nat -> X) :=
   fun n => match n with
-          |0 => x
-          |S n => xi n
-          end.
-
-Notation "s .: sigma" := (scons s sigma) (at level 70).
-
-Definition funcomp {X Y Z} (g : Y -> Z) (f : X -> Y)  :=
-  fun x => g (f x).
+        | 0 => x
+        | S n => xi n
+        end.
 
 
 (** ** Type Class Instances for Notation
@@ -55,21 +50,6 @@ Class Ren4 (X1 X2 X3 X4 : Type) (Y Z : Type) :=
 Class Ren5 (X1 X2 X3 X4 X5 : Type) (Y Z : Type) :=
   ren5 : X1 -> X2 -> X3 -> X4 -> X5 -> Y -> Z.
 
-Notation "s ⟨ xi1 ⟩" := (ren1 xi1 s) (at level 7, left associativity, format "s  ⟨ xi1 ⟩") : subst_scope.
-
-Notation "s ⟨ xi1 ; xi2 ⟩" := (ren2 xi1 xi2 s) (at level 7, left associativity, format "s  ⟨ xi1 ; xi2 ⟩") : subst_scope.
-
-Notation "s ⟨ xi1 ; xi2 ; xi3 ⟩" := (ren3 xi1 xi2 xi3 s) (at level 7, left associativity, format "s  ⟨ xi1 ; xi2 ; xi3 ⟩") : subst_scope.
-
-Notation "s ⟨ xi1 ; xi2 ; xi3 ; xi4 ⟩" := (ren4  xi1 xi2 xi3 xi4 s) (at level 7, left associativity, format "s  ⟨ xi1 ; xi2 ; xi3 ; xi4 ⟩") : subst_scope.
-
-Notation "s ⟨ xi1 ; xi2 ; xi3 ; xi4 ; xi5 ⟩" := (ren5  xi1 xi2 xi3 xi4 xi5 s) (at level 7, left associativity, format "s  ⟨ xi1 ; xi2 ; xi3 ; xi4 ; xi5 ⟩") : subst_scope.
-
-Notation "⟨ xi ⟩" := (ren1 xi) (at level 1, left associativity, format "⟨ xi ⟩") : fscope.
-
-Notation "⟨ xi1 ; xi2 ⟩" := (ren2 xi1 xi2) (at level 1, left associativity, format "⟨ xi1 ; xi2 ⟩") : fscope.
-
-
 
 (** *** Type Classes for Substiution *)
 
@@ -88,22 +68,26 @@ Class Subst4 (X1 X2 X3 X4: Type) (Y Z: Type) :=
 Class Subst5 (X1 X2 X3 X4 X5 : Type) (Y Z: Type) :=
   subst5 : X1 -> X2 -> X3 -> X4 -> X5  -> Y  -> Z.
 
-Notation "s [ sigma ]" := (subst1 sigma s) (at level 7, left associativity, format "s '/' [ sigma ]") : subst_scope.
-
-Notation "s [ sigma ; tau ]" := (subst2 sigma tau s) (at level 7, left associativity, format "s '/' [ sigma ; '/'  tau ]") : subst_scope.
-
 
 (** *** Type Class for Variables *)
 
 Class Var X Y :=
   ids : X -> Y.
 
+Instance idsRen : Var nat nat := id.
+
+(** ** Internal Notation Module so that we can use them here ** **)
+Module InternalNotations.
+
+  Notation "s .: sigma" := (scons s sigma) (at level 70, sigma at next level, right associativity) : subst_scope.
+  Notation "f >> g" := (funcomp g f) (at level 50) : subst_scope.
+
+  Open Scope subst_scope.
+End InternalNotations.
+
 (** ** Proofs for the substitution primitives. *)
+Import InternalNotations.
 
-Arguments funcomp {X Y Z} (g)%fscope (f)%fscope.
-
-Notation "f >> g" := (funcomp g f) (*fun x => g (f x)*) (at level 50) : subst_scope.
-Open Scope subst_scope.
 
 (** A generic lifting of a renaming. *)
 Definition up_ren (xi : nat -> nat) :=
@@ -115,26 +99,43 @@ Lemma up_ren_ren (xi: nat -> nat) (zeta : nat -> nat) (rho: nat -> nat) (E: fora
 Proof.
   intros [|x].
   - reflexivity.
-  - unfold up_ren. simpl. unfold funcomp. rewrite <- E. reflexivity.
-Qed.
-
-(** Eta laws. *)
-Lemma scons_eta_id {n : nat} : var_zero .: shift = id :> (nat -> nat).
-Proof. fext. intros [|x]; reflexivity. Qed.
-
-Lemma scons_eta {T} (f : nat -> T) :
-  f var_zero .: shift >> f = f.
-Proof. fext. intros [|x]; reflexivity. Qed.
-
-Lemma scons_comp (T: Type) U (s: T) (sigma: nat -> T) (tau: T -> U ) :
-  (s .: sigma) >> tau = scons (tau s) (sigma >> tau) .
-Proof.
-  fext. intros [|x]; reflexivity.
+  - unfold up_ren. cbn. unfold funcomp. f_equal. apply E.
 Qed.
 
 (** ** Notations for unscoped syntax *)
+Module UnscopedNotations.
+
+  Include InternalNotations.
+  
+  Notation "s ⟨ xi1 ⟩" := (ren1 xi1 s) (at level 7, left associativity, format "s  ⟨ xi1 ⟩") : subst_scope.
+
+  Notation "s ⟨ xi1 ; xi2 ⟩" := (ren2 xi1 xi2 s) (at level 7, left associativity, format "s  ⟨ xi1 ; xi2 ⟩") : subst_scope.
+
+  Notation "s ⟨ xi1 ; xi2 ; xi3 ⟩" := (ren3 xi1 xi2 xi3 s) (at level 7, left associativity, format "s  ⟨ xi1 ; xi2 ; xi3 ⟩") : subst_scope.
+
+  Notation "s ⟨ xi1 ; xi2 ; xi3 ; xi4 ⟩" := (ren4  xi1 xi2 xi3 xi4 s) (at level 7, left associativity, format "s  ⟨ xi1 ; xi2 ; xi3 ; xi4 ⟩") : subst_scope.
+
+  Notation "s ⟨ xi1 ; xi2 ; xi3 ; xi4 ; xi5 ⟩" := (ren5  xi1 xi2 xi3 xi4 xi5 s) (at level 7, left associativity, format "s  ⟨ xi1 ; xi2 ; xi3 ; xi4 ; xi5 ⟩") : subst_scope.
+
+  Notation "⟨ xi ⟩" := (ren1 xi) (at level 1, left associativity, format "⟨ xi ⟩") : fscope.
+
+  Notation "⟨ xi1 ; xi2 ⟩" := (ren2 xi1 xi2) (at level 1, left associativity, format "⟨ xi1 ; xi2 ⟩") : fscope.
+  
+  Notation "s [ sigma ]" := (subst1 sigma s) (at level 7, left associativity, format "s '/' [ sigma ]") : subst_scope.
+
+  Notation "s [ sigma ; tau ]" := (subst2 sigma tau s) (at level 7, left associativity, format "s '/' [ sigma ; '/'  tau ]") : subst_scope.
+
+  (* Notation "s , sigma" := (scons s sigma) (at level 60, format "s ,  sigma", right associativity) : subst_scope. *)
+
+  Notation "s '..'" := (scons s ids) (at level 1, format "s ..") : subst_scope.
+
+  Notation "↑" := (shift).
+
+End UnscopedNotations.
+
 
 (** Generic fsimpl tactic: simplifies the above primitives in a goal. *)
+(* a.d. TODO some cases for fsimpl use fext *)
 Ltac fsimpl :=
   unfold up_ren; repeat match goal with
          | [|- context[id >> ?f]] => change (id >> f) with f (* AsimplCompIdL *)
@@ -148,15 +149,15 @@ Ltac fsimpl :=
         | [|- context[?f >> (?x .: ?g)]] =>
            change (f >> (x .: g)) with g
          | [|- context[var_zero]] =>  change var_zero with 0
-         | [|- context[?x2 .: shift >> ?f]] =>
-           change x2 with (f 0); rewrite (@scons_eta _ _ f)
+         (* | [|- context[?x2 .: shift >> ?f]] => *)
+         (*   change x2 with (f 0); rewrite (@scons_eta _ _ f) *)
          | [|- context[(?v .: ?g) 0]] =>
            change ((v .: g) 0) with v
          | [|- context[(?v .: ?g) (S ?n)]] =>
            change ((v .: g) (S n)) with (g n)
-         | [|- context[?f 0 .: ?g]] =>
-           change g with (shift >> f); rewrite scons_eta
-         | _ => first [progress (rewrite ?scons_comp) | progress (rewrite ?scons_eta_id)]
+         (* | [|- context[?f 0 .: ?g]] => *)
+         (*   change g with (shift >> f); rewrite scons_eta *)
+         (* | _ => first [progress (rewrite ?scons_comp) | progress (rewrite ?scons_eta_id)] *)
  end.
 
 (** Generic fsimpl tactic: simplifies the above primitives in the context *)
@@ -173,32 +174,22 @@ Ltac fsimplc :=
         | [H: context[?f >> (?x .: ?g)]  |- _] =>
            change (f >> (x .: g)) with g in H
          | [H: context[var_zero]  |- _] =>  change var_zero with 0 in H
-         | [H: context[?x2 .: shift >> ?f]  |- _] =>
-           change x2 with (f 0) in H; rewrite (@scons_eta _ _ f) in H
+         (* | [H: context[?x2 .: shift >> ?f]  |- _] => *)
+         (*   change x2 with (f 0) in H; rewrite (@scons_eta _ _ f) in H *)
          | [H: context[(?v .: ?g) 0]  |- _] =>
            change ((v .: g) 0) with v in H
          | [H: context[(?v .: ?g) (S ?n)]  |- _] =>
            change ((v .: g) (S n)) with (g n) in H
-         | [H: context[?f 0 .: ?g]  |- _] =>
-           change g with (shift >> f); rewrite scons_eta in H
-         | _ => first [progress (rewrite ?scons_comp in *)  | progress (rewrite ?scons_eta_id in *) ]
+         (* | [H: context[?f 0 .: ?g]  |- _] => *)
+         (*   change g with (shift >> f); rewrite scons_eta in H *)
+         (* | _ => first [progress (rewrite ?scons_comp in * )  | progress (rewrite ?scons_eta_id in * ) ] *)
  end.
 
 (** Simplification in both the goal and the context *)
 Tactic Notation "fsimpl" "in" "*" :=
   fsimpl; fsimplc.
 
-(* Notation "s , sigma" := (scons s sigma) (at level 60, format "s ,  sigma", right associativity) : subst_scope. *)
-
-Notation "s '..'" := (scons s ids) (at level 1, format "s ..") : subst_scope.
-Instance idsRen : Var nat nat := id.
-
-Notation "↑" := (shift).
-
 (** ** Tactics for unscoped syntax *)
-
-(** Unfold of the instances in goal and context. *)
-Ltac auto_unfold := unfold subst1, ren1, subst2, ren2, ids; unfold Subst1, Ren1, Subst2, Ren2, Var.
 
 Ltac unfold_funcomp := match goal with
                        | |-  context[(?f >> ?g) ?s] => change ((f >> g) s) with (g (f s))
@@ -209,3 +200,5 @@ Ltac unfold_funcomp := match goal with
 Tactic Notation "auto_case" tactic(t) :=  (match goal with
                                            | [|- forall (i : nat), _] => intros []; t
                                            end).
+
+Arguments funcomp {X Y Z} (g)%fscope (f)%fscope.
