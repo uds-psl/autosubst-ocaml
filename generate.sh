953bc1d93bccf4db5f7d02b6327ab724e6a2d831
Adrian Dapprich <adrian@dapprich.dev>
2021-05-24 23:18:31 +0200

    fix case studies

--- generate.sh
+++ generate.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# # generate code for the example signatures
-# for n in utlc stlc fcbv variadic pi num fol; do
-#     echo dune exec -- bin/main.exe signatures/${n}.sig case-studies/examples/${n}_wellscoped.v coq
-#     dune exec -- bin/main.exe signatures/${n}.sig case-studies/examples/${n}_wellscoped.v coq
-# done
+# generate code for the example signatures
+for n in utlc stlc fcbv variadic pi num; do
+    echo dune exec -- bin/main.exe signatures/${n}.sig case-studies/examples/${n}_wellscoped.v coq ge810
+    dune exec -- bin/main.exe signatures/${n}.sig case-studies/examples/${n}_wellscoped.v coq ge810
+done
 
-# for n in utlc stlc fcbv pi num; do
-#     echo dune exec -- bin/main.exe signatures/${n}.sig case-studies/examples/${n}_unscoped.v ucoq
-#     dune exec -- bin/main.exe signatures/${n}.sig case-studies/examples/${n}_unscoped.v ucoq
-# done
+# just for fol because it uses the cod functor, so it needs functional extensionality 
+echo dune exec -- bin/main.exe signatures/fol.sig case-studies/examples/fol_wellscoped.v coq ge810 false
+dune exec -- bin/main.exe signatures/fol.sig case-studies/examples/fol_wellscoped.v coq ge810 false
+
+for n in utlc stlc fcbv pi num; do
+    echo dune exec -- bin/main.exe signatures/${n}.sig case-studies/examples/${n}_unscoped.v ucoq ge810
+    dune exec -- bin/main.exe signatures/${n}.sig case-studies/examples/${n}_unscoped.v ucoq ge810
+done
 
 # generate the code for Kathrin's case study.
 # Since I don't generate tactics and typeclasses I used the ones from Kathrin and append them to the code I generated
@@ -18,23 +22,17 @@ KAT="case-studies/kathrin/coq/"
 generate_file() {
     file=$1
     scope=$2
-    echo dune exec -- bin/main.exe ${KAT}${file}.sig ${KAT}${file}.v ${scope}
-    dune exec -- bin/main.exe ${KAT}${file}.sig ${KAT}${file}.v ${scope}
-}
-append_tactics() {
-    file=$1
-    echo cat ${KAT}${file}_axioms.v ${KAT}${file}_rest.v > ${KAT}${file}_axioms.v
-    cat ${KAT}${file}_axioms.v ${KAT}${file}_rest.v > ${KAT}${file}_axioms.v
+    echo dune exec -- bin/main.exe ${KAT}${file}.sig ${KAT}${file}.v ${scope} ge810 false false
+    dune exec -- bin/main.exe ${KAT}${file}.sig ${KAT}${file}.v ${scope} ge810 false false
 }
 
-# echo cp data/core.v data/core_axioms.v data/fintype_809.v data/unscoped_809.v ${KAT}
-# cp data/core.v data/core_axioms.v data/fintype_809.v data/unscoped_809.v ${KAT}
+echo cp data/core_809.v data/core_axioms.v data/fintype.v data/fintype_axioms.v data/unscoped.v data/unscoped_axioms.v ${KAT}
+cp data/core_axioms.v data/fintype.v data/fintype_axioms.v data/unscoped.v data/unscoped_axioms.v ${KAT}
+cp data/core_809.v ${KAT}core.v
 
 # the case study only contains one instance of unscoped code
 generate_file "Chapter3/utlc_pure" ucoq
-# append_tactics "Chapter3/utlc_pure"
 
 for f in "Chapter3/utlc_scoped" "Chapter6/utlc_pairs" "Chapter6/sysf_cbv" "Chapter6/variadic_fin" "Chapter9/stlc" "Chapter10/sysf" "Chapter10/sysf_pat"; do
     generate_file $f coq
-    # append_tactics $f
 done
