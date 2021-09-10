b3708d1db1e3ce1a100797541ab751d0a007f7ae
Adrian Dapprich <adrian@dapprich.dev>
2021-09-10 15:53:10 +0200

    cleanup

--- case-studies/kathrin/coq/fintype.v
+++ case-studies/kathrin/coq/fintype.v
@@ -363,7 +363,7 @@ Ltac fsimpl :=
          | [|- context[?tau (scons_p ?p ?f ?g _)]] => (rewrite_strat innermost scons_p_comp'); eta_reduce
          | [|- context[scons (@var_zero ?n) shift]] => change (scons (@var_zero n) shift) with (fun x => (scons (@var_zero n) shift) x); setoid_rewrite scons_eta_id'; eta_reduce
          (* | _ => progress autorewrite with FunctorInstances *)
-         | _ => first [progress setoid_rewrite scons_p_head' | progress setoid_rewrite scons_p_tail' ]; idtac "head tail"
+         | _ => first [progress setoid_rewrite scons_p_head' | progress setoid_rewrite scons_p_tail' ]
          end.
 
 (** Generic fsimpl tactic: simplifies the above primitives in the context *)
