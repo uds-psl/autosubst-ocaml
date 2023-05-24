59c4663e513f2dabec60ccb88025b3aea10cb435
Yannick Forster <yannick.forster@inria.fr>
2023-05-24 20:30:22 +0100

    fix CI

--- case-studies/prelude/prelude.v
+++ case-studies/prelude/prelude.v
@@ -1,4 +1,6 @@
-Require Import fintype unscoped.
-Require Import Setoid.
+Require Import core fintype unscoped.
+
+Require Import core_axioms unscoped_axioms.
+Require Import Setoid Morphisms Relation_Definitions.
 
 Inductive test : Type := .
--- signatures/prelude.sig
+++ signatures/prelude.sig
@@ -2,3 +2,5 @@ test : Type
 term : Type
 
 C : test -> term
+lam : (bind term in term) -> term
+app : term -> term -> term
